5e5fed33924cf69685cecc2451a4769b48d7e197
hyeon48165 <hyeon48615@gmail.com>
2025-12-07 12:44:15 +0900

    [#1] Chore: 프로젝트 초기설정

---
+++ src/main/java/org/todaybook/embedding/BookEmbeddingWorkerApplication.java
@@ -0,0 +1,13 @@
+package org.todaybook.embedding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookEmbeddingWorkerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BookEmbeddingWorkerApplication.class, args);
+  }
+
+}
