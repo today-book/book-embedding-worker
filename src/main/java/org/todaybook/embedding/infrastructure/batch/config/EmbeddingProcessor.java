b240c7eb5baac8095ea13a37ebe3789366a193c3
hyeon48165 <hyeon48615@gmail.com>
2025-12-07 19:41:42 +0900

    [#2] Feat: Spring Batch Processor 구현
    - Book 데이터를 id, content, metadata로 변환

---
+++ src/main/java/org/todaybook/embedding/infrastructure/batch/config/EmbeddingProcessor.java
@@ -0,0 +1,45 @@
+package org.todaybook.embedding.infrastructure.batch.config;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.item.ItemProcessor;
+import org.todaybook.embedding.domain.Book;
+import org.todaybook.embedding.domain.VectorBook;
+
+@RequiredArgsConstructor
+public class EmbeddingProcessor implements ItemProcessor<Book, VectorBook> {
+
+  @Override
+  public VectorBook process(Book item) throws Exception {
+    return new VectorBook(
+        item.id(),
+        getContent(item),
+        getMetadata(item)
+    );
+  }
+
+  private static String getContent(Book book) {
+    StringBuilder builder = new StringBuilder();
+    builder.append("title: ").append(book.title()).append("\n");
+    builder.append("author: ").append(book.author()).append("\n");
+    builder.append("description: ").append(book.description()).append("\n");
+
+    String categories = book.categories() != null ? String.join(",", book.categories()) : "";
+    builder.append("categories: ").append(categories).append("\n");
+
+    return builder.toString();
+  }
+
+  private static Map<String, Object> getMetadata(Book book) {
+    Map<String, Object> metadata = new HashMap<>();
+    metadata.put("bookId", book.id());
+    metadata.put("title", book.title());
+    metadata.put("author", book.author());
+    metadata.put("categories", book.categories() != null ? book.categories() : List.of());
+
+
+    return metadata;
+  }
+}
