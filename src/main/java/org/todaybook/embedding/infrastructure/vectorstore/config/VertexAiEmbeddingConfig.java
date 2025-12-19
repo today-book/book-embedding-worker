839fc7316d52819ad6c260aee6e7b925c50af147
hyeon48165 <hyeon48615@gmail.com>
2025-12-19 18:00:22 +0900

    Ci: 개발환경 배포 설정

--- src/main/java/org/todaybook/embedding/infrastructure/vectorstore/config/VertexAiEmbeddingConfig.java
+++ src/main/java/org/todaybook/embedding/infrastructure/vectorstore/config/VertexAiEmbeddingConfig.java
@@ -3,16 +3,24 @@ package org.todaybook.embedding.infrastructure.vectorstore.config;
 import org.springframework.ai.vertexai.embedding.VertexAiEmbeddingConnectionDetails;
 import org.springframework.ai.vertexai.embedding.text.VertexAiTextEmbeddingModel;
 import org.springframework.ai.vertexai.embedding.text.VertexAiTextEmbeddingOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class VertexAiEmbeddingConfig {
+
+  @Value("${spring.ai.vertex.ai.embedding.project-id}")
+  private String projectId;
+
+  @Value("${spring.ai.vertex.ai.embedding.location}")
+  private String location;
+
   @Bean
   public VertexAiEmbeddingConnectionDetails vertexAiEmbeddingConnectionDetails() {
     return VertexAiEmbeddingConnectionDetails.builder()
-        .projectId("todaybook-479908")
-        .location("us-central1")
+        .projectId(projectId)
+        .location(location)
         .build();
   }
 
